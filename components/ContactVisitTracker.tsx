2de6e665160e9783c3f1f52e80be1e4b656125cb
iticcotx <iticcotx@gmail.com>
2026-08-06 18:09:58 -0500

    Add Ennis SEO and conversion improvements

---
+++ components/ContactVisitTracker.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+declare global { interface Window { gtag?: (...args: unknown[]) => void; } }
+
+export default function ContactVisitTracker() {
+  useEffect(() => {
+    window.gtag?.("event", "contact_page_visit", { page_path: window.location.pathname });
+  }, []);
+  return null;
+}
+
